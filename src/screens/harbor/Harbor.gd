16f3c43ce9f7bf69635aade84a4a8e245f9820df
Michael Binder <rednib.coding@gmail.com>
2023-01-18 17:32:28 +0100

    Added ship-/shipyard buttons to open/close ship-/shipyard windows

--- src/screens/harbor/Harbor.gd
+++ src/screens/harbor/Harbor.gd
@@ -11,6 +11,11 @@ func _ready():
 	$HarborName/Label.text = Harbor.Names[Harbor.current]
 	$Day/Label.text = str(Player.currentDay)
 
+	$ShipWindow.visible = true
+	$ShipButton.visible = false
+	$ShipyardWindow.visible = false
+	$ShipyardButton.visible = true
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -20,3 +25,17 @@ func _process(_delta):
 func _on_MapButton_button_up():
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://screens/worldmap/WorldMap.tscn")
+
+
+func _on_ShipyardButton_button_up():
+	$ShipWindow.visible = false
+	$ShipButton.visible = true
+	$ShipyardWindow.visible = true
+	$ShipyardButton.visible = false
+
+
+func _on_ShipButton_button_up():
+	$ShipWindow.visible = true
+	$ShipButton.visible = false
+	$ShipyardWindow.visible = false
+	$ShipyardButton.visible = true
